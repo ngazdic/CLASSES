38af8b9cd70199a019ef0414753b50b74efe4b14
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-13 15:13:55 -0400

    class properties set in main

---
+++ CLASSES/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CLASSES
+{
+    public class Car
+    {
+        public Car() //construcotr is defalut
+        {
+        }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string Year { get; set; }
+
+
+
+
+    }
+
+}
+
---
+++ CLASSES/Program.cs
@@ -0,0 +1,11 @@
+﻿using CLASSES;
+
+Car car = new Car();
+
+car.Make = "Renalut";
+car.Model = "Renalut 4";
+car.Year = "1988";
+
+Console.WriteLine(car.Make);
+Console.WriteLine(car.Model);
+Console.WriteLine(car.Year);
